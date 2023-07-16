df4e2cb01c55b8c63239418e31414b558bea3d4d
Ronny <ronnydougla2@gmail.com>
2023-07-16 17:29:58 -0300

    feature/script.js

--- script.js
+++ script.js
@@ -7,6 +7,49 @@ const seaBoard = [
   [".", ".", ".", ".", "."],
 ];
 
-/*
-  Desenvolva seu código logo abaixo
-*/
+// Desenvolva seu código logo abaixo
+
+// Função para rodar testes allocateShips
+allocateShips = (shipPositions, initialBoard) => {
+  const board = initialBoard;
+
+  for (let [row, column] of shipPositions) {
+    if (board[row] && board[row][column]) {
+      board[row][column] = "S";
+    } else {
+      board[row][column] = ".";
+    }
+  }
+  return board;
+};
+
+console.log(allocateShips);
+
+
+// Função para rodar testes checkGuesses
+checkGuesses = (guesses, mountedBoard) => {
+  const board = mountedBoard; 
+
+  for (let [row, column] of guesses) {
+    if (board[row] && board[row][column] === "S") {
+      board[row][column] = "X";
+    }
+  }
+  return board;
+};
+
+console.log(checkGuesses);
+
+// Função para rodar testes checkWinCondition 
+
+checkWinCondition = (guessedBoard) => {
+  for(let row of guessedBoard){
+    for(let column of row){
+      if(column === "S"){
+        return false
+      }
+    }
+  }
+  return true
+};
+
