95cb0e7ca5fc0edb8a2195507f2280b6af7020fa
Ronny Douglas <80992498+ronnydouglasjr@users.noreply.github.com>
2023-07-16 15:59:55 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// Tabuleiro inicial de exemplo
+const seaBoard = [
+  [".", ".", ".", ".", "."],
+  [".", ".", ".", ".", "."],
+  [".", ".", ".", ".", "."],
+  [".", ".", ".", ".", "."],
+  [".", ".", ".", ".", "."],
+];
+
+/*
+  Desenvolva seu código logo abaixo
+*/
